637f0b71cf2da5beab5a05041969c5e91f275198
Shezad <shezadmohamed@gmail.com>
2024-11-11 21:59:02 +0000

    Complete chapter one and update .gitignore

---
+++ HaskellData01.hs
@@ -0,0 +1,20 @@
+module HaskellData01 where
+import Data.List
+
+median :: [Double] -> Double
+median xs = if oddInLength then
+              middleValue
+            else
+              (middleValue + beforeMiddleValue) / 2
+  where
+    sortedList = sort xs
+    oddInLength = 1 == mod (genericLength xs) 2
+    middle = floor $ (genericLength xs) / 2
+    middleValue = genericIndex sortedList middle
+    beforeMiddleValue = genericIndex sortedList (middle - 1)
+
+
+-- Finds the indices of every vowel in a word
+vowelIndices :: String -> [Integer]
+vowelIndices word =
+  map fst $ filter ( \(_, letter) -> elem letter "aeiouAEIOU") $ zip [1..] word 
